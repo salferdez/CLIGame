27b73298d053bfffb44867f927ac0875a8640f1e
salferdez <151395807+salferdez@users.noreply.github.com>
2025-02-19 23:43:35 +0100

    Added haddock comments

--- src/Character.hs
+++ src/Character.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+-- |
+-- Module      :  CLIGame.Character
+-- Maintainer  :  Salvador Fernández <salferdez@gmail.com>
 module Character
   ( Character (..),
     -- Lenses
@@ -16,52 +19,52 @@ module Character
 where
 
 import Control.Lens
+import Text.Read (readMaybe)
 
-data CharacterClass = FIGHTER | ROGUE | SORCERER | MUMMY deriving (Show, Eq)
+-- | Class of character, read instance needed for readMaybe
+data CharacterClass
+  = FIGHTER
+  | ROGUE
+  | SORCERER
+  | -- | Used for testing, has 1 HP
+    MUMMY
+  deriving (Show, Eq, Read)
 
--- Needed for readMaybe in Game module
-instance Read CharacterClass where
-  readsPrec _ str =
-    case str of
-      "FIGHTER" -> [(FIGHTER, "")]
-      "ROGUE" -> [(ROGUE, "")]
-      "SORCERER" -> [(SORCERER, "")]
-      "MUMMY" -> [(MUMMY, "")]
-      _ -> []
-
+-- | Represents player's character
 data Character = Character
   { _name :: String,
     _hp :: Int,
     _minDamage :: Int,
     _maxDamage :: Int,
-    _CharacterClass :: CharacterClass
+    _characterClass :: CharacterClass
   }
   deriving (Show, Eq)
 
 makeLenses ''Character
 
+-- | Creates new Character based on given name and CharcterClass
 initializeCharacter :: String -> CharacterClass -> Character
-initializeCharacter name characterClass
-  | characterClass == FIGHTER = Character {_name = name, _hp = 100, _minDamage = 3, _maxDamage = 8, _CharacterClass = characterClass}
-  | characterClass == SORCERER = Character {_name = name, _hp = 100, _minDamage = 2, _maxDamage = 12, _CharacterClass = characterClass}
-  | characterClass == ROGUE = Character {_name = name, _hp = 100, _minDamage = 3, _maxDamage = 6, _CharacterClass = characterClass}
-  | characterClass == MUMMY = Character {_name = name, _hp = 1, _minDamage = 0, _maxDamage = 0, _CharacterClass = characterClass}
+initializeCharacter name FIGHTER = Character {_name = name, _hp = 100, _minDamage = 3, _maxDamage = 8, _characterClass = FIGHTER}
+initializeCharacter name SORCERER = Character {_name = name, _hp = 100, _minDamage = 2, _maxDamage = 12, _characterClass = SORCERER}
+initializeCharacter name ROGUE = Character {_name = name, _hp = 100, _minDamage = 3, _maxDamage = 6, _characterClass = ROGUE}
+initializeCharacter name MUMMY = Character {_name = name, _hp = 1, _minDamage = 0, _maxDamage = 0, _characterClass = MUMMY}
 
+-- | Asks player for character name
+-- Calls setCharacter for handling correct class input
 playerCharacter :: IO Character
 playerCharacter = do
   putStrLn "Insert your character's name: "
   name <- getLine
   setCharacter name
 
+-- | Asks player for CharacterClass and calls initializeCharacter if ok.
+-- Else, retries until valid CharacterClass is entered
 setCharacter :: String -> IO Character
 setCharacter name = do
-  putStrLn "Insert your character's class: FIGHTER | SORCERER | ROGUE"
+  putStrLn "Insert your character's class: FIGHTER | SORCERER | ROGUE | MUMMY"
   classInput <- getLine
-  case classInput of
-    "FIGHTER" -> return $ initializeCharacter name FIGHTER
-    "SORCERER" -> return $ initializeCharacter name SORCERER
-    "ROGUE" -> return $ initializeCharacter name ROGUE
-    "MUMMY" -> return $ initializeCharacter name MUMMY
-    _ -> do
+  case readMaybe classInput of
+    Just characterClass -> return $ initializeCharacter name characterClass
+    Nothing -> do
       putStrLn "Command not accepted. Try again."
-      playerCharacter
+      setCharacter name
