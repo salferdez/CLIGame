9fa148cba0a3f2c710f1fe7395de6fda7e8c9484
salferdez <151395807+salferdez@users.noreply.github.com>
2025-02-19 23:46:57 +0100

    Added haddock comments to Monster.hs

--- src/Monster.hs
+++ src/Monster.hs
@@ -1,5 +1,8 @@
 {-# LANGUAGE TemplateHaskell #-}
 
+-- |
+-- Module      :  CLIGame.Character
+-- Maintainer  :  Salvador Fernández <salferdez@gmail.com>
 module Monster
   ( Monster (..),
     -- Lenses
@@ -25,6 +28,7 @@ import Data.Maybe (mapMaybe)
 import System.Random
 import Text.Read (readMaybe)
 
+-- | Represents player's enemies
 data Monster = Monster
   { _monsterName :: String,
     _minHP :: Int,
@@ -35,7 +39,7 @@ data Monster = Monster
 
 makeLenses ''Monster
 
--- Mummy instance
+-- | Mummy instance
 goblin :: Monster
 goblin =
   Monster
@@ -46,14 +50,14 @@ goblin =
       _maxMonsterDamage = 9
     }
 
--- Used for reading monster from file, splitting each file and storing them in a list
+-- | Splits string in list of strings according to a delimiter (char)
 splitOn :: Char -> String -> [String]
 splitOn delim s = foldr go [""] s
   where
     go _ [] = [""]
     go c (x : xs) = if c == delim then "" : x : xs else (c : x) : xs
 
--- Parsing line in file, getting a monster
+-- | Parsing line in file, splitting on \'\:\', getting a monster
 parseMonster :: String -> Maybe Monster
 parseMonster line =
   case splitOn ':' line of
@@ -65,14 +69,14 @@ parseMonster line =
         <*> readMaybe maxMonsterDamage
     _ -> Nothing
 
--- Parsing file content, getting list of all monsters
+-- | Parsing file content (valid lines are those that don't start with '#' or blank space), getting list of all monsters
 parseMonsters :: String -> [Monster]
 parseMonsters input =
   let lines' = lines input
       validLines = filter (\line -> not (null line) && not ("#" `isPrefixOf` line)) lines'
    in mapMaybe parseMonster validLines
 
--- Read file, parse monsters, return list of monsters
+-- | Read file, parse monsters, return list of monsters
 readMonsters :: IO [Monster]
 readMonsters = do
   let pathFile = "./src/monsters.txt"
@@ -80,17 +84,18 @@ readMonsters = do
   let monsters = parseMonsters content
   return monsters
 
--- Used for choosing random monster from list
+-- | Used for choosing random monster from list
 chooseRandom :: [a] -> IO (Maybe a)
 chooseRandom [] = return Nothing
 chooseRandom xs = do
   index <- randomRIO (0, length xs - 1)
   return (Just (xs !! index))
 
+-- | Used to simplify treatment of values
 extract :: IO (Maybe a) -> IO a
 extract ioMaybe = do
   maybeVal <- ioMaybe
   case maybeVal of
     Just x -> return x
-    -- This SHOULD never happen
+    -- This should never happen
     Nothing -> error "extract encountered Nothing"
